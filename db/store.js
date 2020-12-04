00b21f2802b5da376db312710453cd4de533e19f
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-03 23:49:45 -0600

    updating store.js

--- db/store.js
+++ db/store.js
@@ -10,3 +10,14 @@ class Store{
     findAllDepartments() {
         return this.connection.query("SELECT * FROM departments")
     }
+    findAllRoles() {
+        return this.connection.query("SELECT * from roles")
+    }
+    addEmployee(response) {
+        return this.connection.query("INSERT INTO employees SET ?", {
+            first_name: response.firstName,
+            last_name: response.lastName,
+            role_id: response.roleID, 
+            manager_id: response.managerID,
+        });
+    }
