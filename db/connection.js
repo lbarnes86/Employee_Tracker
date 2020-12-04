e2ca368601a250d9a14c6e29988f22a3d59aad56
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-03 23:28:38 -0600

    updating connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,13 @@
 let mysql = require("mysql");
 let util = require("util");
 
+const connection = mysql.createConnection ({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "employee_trackerDB"
+})
+connection.connect();
+connection.query = util.promisify(connection.query);
+
+module
