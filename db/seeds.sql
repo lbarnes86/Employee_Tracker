004c3b38028c8d237bb64ca7fc604cc52f87bd63
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-03 23:44:59 -0600

    updating schema.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Lloyd", "Barnes", 3454, 323);
+
+INSERT INTO departments (name)
+VALUES ("sales");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("manager", 82000.00, 323)
