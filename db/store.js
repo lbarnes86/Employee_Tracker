354eb77ea1c19b05c5b57849de0d092c422cf131
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-03 23:49:11 -0600

    updating store.js

--- db/store.js
+++ db/store.js
@@ -1,1 +1,12 @@
-let 
+let connection = require("./connection");
+
+class Store{
+    constructor(connection){
+        this.connection = connection
+    }
+    findAllEmployees() {
+        return this.connection.query("SELECT * FROM employees")
+    }
+    findAllDepartments() {
+        return this.connection.query("SELECT * FROM departments")
+    }
