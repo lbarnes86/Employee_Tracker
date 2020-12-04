67656fb79c6c9452a7865661b554d31fd69557f6
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-03 23:41:05 -0600

    updating schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,25 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
-CREATE TABLE departments (
+CREATE TABLE employees (
     id INT AUTO_INCREMENT,
-    name VARCHAR(40),
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    role_id INT,
+    manager_id INT,
     PRIMARY KEY(id)
 );
-CREATE TABLE roles (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(40),
-    PRIMARY KEY(id)
-);
+
 CREATE TABLE departments (
     id INT AUTO_INCREMENT,
     name VARCHAR(40),
     PRIMARY KEY(id)
 );
 
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(40),
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY(id)
+);
