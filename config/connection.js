f0ce6396d593a39f12ea7aacd1ddf803765a836b
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-05 14:53:11 -0600

    updated code for connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,5 @@ const mysql = require('mysql')
 // require util for connection to mysql
 const util = require ('util');
 
-
-let config = {
-    host: "localhost",
-    port: 8080,
-    user: "root",
-    password: "rootroot",
-    database: "employee_trackerDB"
-};
-
-module.exports = config;
+const connection = mysql.createConnection({
+    host: 'localhost',
